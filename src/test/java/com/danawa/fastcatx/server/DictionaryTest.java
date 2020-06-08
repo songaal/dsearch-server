46def4662d40cb02d50753837734365df96e4f50
joonwoo8888 <dev.joonwoo@gmail.com>
2020-06-08 18:01:35 +0900

    .

--- src/test/java/com/danawa/fastcatx/server/DictionaryTest.java
+++ src/test/java/com/danawa/fastcatx/server/DictionaryTest.java
@@ -11,16 +11,16 @@ import java.io.IOException;
 @SpringBootTest
 public class DictionaryTest {
 
-    @Autowired
-    private DictionaryService dictionaryService;
+//    @Autowired
+//    private DictionaryService dictionaryService;
 
     @Test
     public void downloadTest() {
-        try {
-            dictionaryService.findAll("USER");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            dictionaryService.findAll("USER");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
 
     }
