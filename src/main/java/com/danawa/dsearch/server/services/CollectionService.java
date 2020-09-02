4b191b3691cff21274ed4222595702c76360109d
sunjiho <sunjiho@danawa.com>
2020-09-02 14:02:51 +0900

    컬렉션 내용 변경시 컬렉션 이름 null로 바뀌는 것 수정

--- src/main/java/com/danawa/dsearch/server/services/CollectionService.java
+++ src/main/java/com/danawa/dsearch/server/services/CollectionService.java
@@ -441,7 +441,8 @@ public class CollectionService {
         try (RestHighLevelClient client = elasticsearchFactory.getClient(clusterId)) {
             GetResponse getResponse = client.get(new GetRequest().index(collectionIndex).id(id), RequestOptions.DEFAULT);
             Map<String, Object> sourceAsMap = getResponse.getSourceAsMap();
-            sourceAsMap.put("name", collection.getName());
+            if(collection.getName() != null && collection.getName().length() > 0)
+                sourceAsMap.put("name", collection.getName());
             sourceAsMap.put("cron", collection.getCron());
             sourceAsMap.put("sourceName", collection.getSourceName());
             sourceAsMap.put("jdbcId", collection.getJdbcId());
