f926ea8faa4f960627b59a6bd4f980dea6e6c76c
sunjiho <sunjiho@gitlab.com>
2020-12-14 15:02:52 +0900

    kibana 연결테스트

---
+++ src/main/java/com/danawa/dsearch/server/controller/KibanaController.java
@@ -0,0 +1,31 @@
+package com.danawa.dsearch.server.controller;
+
+import com.danawa.dsearch.server.entity.Cluster;
+import com.danawa.dsearch.server.excpetions.NotFoundException;
+import org.springframework.http.*;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/kibana")
+public class KibanaController {
+
+    @PostMapping("/status")
+    public ResponseEntity<?> getKibanaStatus(@RequestBody Map<String, Object> url) throws NotFoundException {
+
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setConnectTimeout(5000);
+        factory.setConnectionRequestTimeout(5000);
+        RestTemplate template = new RestTemplate(factory);
+
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+
+        ResponseEntity responseEntity = template.exchange((String) url.get("url"), HttpMethod.GET, entity, Map.class);
+        return new ResponseEntity<>(responseEntity.getBody(), HttpStatus.OK);
+    }
+}
