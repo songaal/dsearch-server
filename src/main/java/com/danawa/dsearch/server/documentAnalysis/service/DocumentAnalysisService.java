73454e70f4cce7c65ac86518f9bdf354eaaa19f3
sunjiho <sunjiho@gitlab.com>
2023-01-02 17:46:53 +0900

    refactor: 문서분석 쿼리 조회 갯수 100건으로 줄임

--- src/main/java/com/danawa/dsearch/server/documentAnalysis/service/DocumentAnalysisService.java
+++ src/main/java/com/danawa/dsearch/server/documentAnalysis/service/DocumentAnalysisService.java
@@ -56,7 +56,7 @@ public class DocumentAnalysisService {
 
             // 5. 쿼리와 field 리스트를 합치기
             Map<String, Object> queryMap = JsonUtils.convertStringToMap(query);
-            queryMap.put("size", 1000); // 5-1. 1만건이 쿼리는 너무 많은 리소스를 소모하므로 1000건만
+            queryMap.put("size", 100); // 5-1. 1만건 쿼리는 너무 많은 리소스를 소모하므로 100건만
             queryMap.put("_source", extractedFields);
 
             // 6. 문자열 형태로 변환 후 검색
